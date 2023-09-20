a5f846a67405d733cea49b3391b99d20c4e622b1
Michael <michael@kohlercia.com.br>
2023-09-20 16:40:48 -0300

    atualizado telas

--- principal.php
+++ principal.php
@@ -12,9 +12,9 @@
 <?php
 // Verifique se a variável 'user' foi enviada através do método POST
 if (isset($_POST['user'])) {
-    $profileName = $_POST['user'];
+    $user = $_POST['user'];
 } else {
-    $profileName = "Anônimo";
+    $user = "Anônimo";
 }
 ?>
 
@@ -26,7 +26,7 @@ if (isset($_POST['user'])) {
                 <div class="linktext">Home</div>
             </a>
             <a class="item" href="#nav-6">
-                <div class="linktext"><?php echo $profileName; ?></div>
+                <div class="linktext"><?php echo $user; ?></div>
             </a>
             <a class="item" href="#nav-6">
                 <div class="linktext">Sair</div>
@@ -35,12 +35,12 @@ if (isset($_POST['user'])) {
     </section>
 
     <div class="container">
-        <div class="card">
+        <div class="card" onclick="submitForm('financas.php')">
             <div class="card__image-container">
+                <img src="img/dinheiro.png" alt="Ícone Dinheiro" class="icon-card">
             </div>
 
             <svg class="card__svg" viewBox="0 0 800 500">
-
                 <path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"></path>
                 <path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"></path>
             </svg>
@@ -51,22 +51,41 @@ if (isset($_POST['user'])) {
             </div>
         </div>
 
-
-        <div class="card">
+        <div class="card" onclick="submitForm('senhas.php')">
             <div class="card__image-container">
+                <img src="img/cadeado.png" alt="Ícone Cadeado" class="icon-card">
             </div>
 
             <svg class="card__svg" viewBox="0 0 800 500">
-
                 <path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"></path>
                 <path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"></path>
             </svg>
 
             <div class="card__content">
                 <p class="card__title">Senhas</p>
-                <p>Gerenciador de Senhas, para todos termos acesso compartilhado as senhas dos serviços de streaming.</p>
+                <p>Gerenciador de Senhas, para todos termos acesso compartilhado às senhas dos serviços de streaming.</p>
             </div>
         </div>
     </div>
+
+    <form id="hiddenForm" method="post">
+        <input type="hidden" name="user" id="user" value="">
+        <input type="hidden" name="valor" value="1">
+    </form>
+
+    <script>
+        function submitForm(action) {
+            // Define o valor de user no campo hidden
+            document.getElementById('user').value = '<?php echo $user; ?>';
+
+            // Define a ação (URL) do formulário com base no card clicado
+            document.getElementById('hiddenForm').action = action;
+
+            // Submete o formulário
+            document.getElementById('hiddenForm').submit();
+        }
+    </script>
 </body>
+
+
 </html>
